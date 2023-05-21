03a2df4191d976eb53715beffc9c9512f9baa34e
danibog <dannybg090909@gmail.com>
2023-05-21 12:09:15 +0300

    ui cart modified

--- src/components/Musics/Musics.js
+++ src/components/Musics/Musics.js
@@ -2,33 +2,12 @@ import MusicList from "./MusicList";
 
 import MainSection from "../UI/MainSectionContainer";
 
-const DummyMusics = [
-  {
-    artist: "The Weekend",
-    songName: "Happy",
-    songDescription:
-      " Lorem apiente libero quam tempore exercitationem ex odio maxime fugaodiPariatur quos aliquid quae",
-    id: "m1",
-    photoLink:
-      "https://www.rollingstone.com/wp-content/uploads/2020/02/TheWeeknd.jpg?w=1581&h=1054&crop=1",
-  },
-  {
-    artist: "The Weekend",
-    songName: "Drunk",
-    songDescription:
-      " Lorem apiente libero quam tempore exercitationem ex odio maxime fugaodiPariatur quos aliquid quae",
-    id: "m2",
-    photoLink:
-      "https://www.goldderby.com/wp-content/uploads/2020/11/the-weeknd.jpg?w=620&h=360&crop=1",
-  },
-];
-
-const Musics = () => {
+const Musics = (props) => {
   return (
     <MainSection>
       <div>
         <h3>Suggested Music</h3>
-        <MusicList type="suggestedMusic" musics={DummyMusics}></MusicList>
+        <MusicList type="suggestedMusic" musics={props.DummyMusics}></MusicList>
       </div>
 
       <div>
